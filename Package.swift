2dd9d4f5b6bdb2640829c65957dfb451d90e86a1
Jorge <jorge@radbench.com>
2021-01-05 10:22:10 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,16 @@ import PackageDescription
 let package = Package(
     name: "leaf",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
+        .iOS(.v13)
     ],
     products: [
         .library(name: "Leaf", targets: ["Leaf"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+//        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/radbench/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(name: "Leaf", dependencies: [
